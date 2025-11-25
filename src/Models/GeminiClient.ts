7ed3522ef077d728542e3dbc97a7ce948d302a6c
ALi Gharian <ali.gharian98@gmail.com>
2025-11-25 14:52:04 +0330

    add date to ai

--- src/Models/GeminiClient.ts
+++ src/Models/GeminiClient.ts
@@ -108,6 +108,15 @@ export class GeminiClient {
   async generateText(options: GenerateOptions): Promise<void> {
     const model = options.model ?? "gemini-2.5-flash";
 
+    // add the today date to the
+    const today = new Date();
+    const year = today.getUTCFullYear();
+    const month = String(today.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(today.getUTCDate()).padStart(2, "0");
+    const currentDateString = `${year}-${month}-${day}`;
+
+    // -------------------------------------------------------
+
     // const countTokensResponse = await this.client.models.countTokens({
     //   model: model,
     //   contents: options.prompt,
@@ -117,19 +126,32 @@ export class GeminiClient {
     const SYSTEM_INSTRUCTION: ContentUnion = [
       "You are SafeGPT, the official assistant of SafeBroker.org.",
 
-      // 1. LANGUAGE PROTOCOL (قانون زبان - بسیار مهم)
+      // 1. LANGUAGE PROTOCOL
       "LANGUAGE RULE: You MUST respond in the same language as the user. If the user writes in Persian (Farsi), your response MUST be in Persian.",
       "If the input is mixed (Persian + English terms), reply in Persian.",
 
-      // 2. CONCISENESS (کوتاه‌نویسی)
+      // 2. CONCISENESS
       "CORE RULE: Be extremely concise. Keep answers under 3-4 sentences.",
 
-      // 3. ENGAGEMENT (درگیر کردن کاربر)
+      // 3. ENGAGEMENT
       "ENGAGEMENT: Never end with a full stop. Always end with a relevant follow-up question or a suggestion to keep the conversation going.",
 
-      // 4. TOOLS (ابزارها)
+      // 4. TOOLS
       "If user asks questions about the current webpage, call the scrapePage action.",
+
+      // 5. ADD CURRENT DATE
+      `
+      [CONTEXTUAL_RULES]
+      **تاریخ امروز به فرمت YYYY-MM-DD عبارت است از: ${currentDateString}**
+      `,
     ];
+
+    //  **قوانین فراخوانی تابع getForexEconomicNews:**
+    //   1. تو باید عبارت های زمانی نسبی (مثل "این هفته" یا "هفته آینده") را با استفاده از تاریخ امروز، به محدوده تاریخ دقیق YYYY-MM-DD تبدیل کنی. شروع هفته را روز **دوشنبه** در نظر بگیر.
+    //   2. کدهای ارز (مثل یورو، دلار) را به کدهای کشور زیر نگاشت کن و به پارامتر countryCodes بفرست:
+    //     یورو/EUR -> EU | دلار آمریکا/USD -> US | پوند/GBP -> GB | ین/JPY -> JP | دلار کانادا/CAD -> CA | دلار استرالیا/AUD -> AU | فرانک سوئیس/CHF -> CH | دلار نیوزیلند/NZD -> NZ.
+    //   [/CONTEXTUAL_RULES]
+
     const functionDeclarations: FunctionDeclaration[] = [
       {
         name: "scrapePage",
