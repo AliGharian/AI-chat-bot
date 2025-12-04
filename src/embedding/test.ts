ddaf85f3680949fa1d904cc0c53dd25b78073049
ALi Gharian <ali.gharian98@gmail.com>
2025-12-04 11:13:23 +0330

    test embedding to redsi

--- src/embedding/test.ts
+++ src/embedding/test.ts
@@ -1,4 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { RedisVectorStore } from "@langchain/redis";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { Document } from "langchain";
+import { createClient } from "redis";
 
 // ⚠️ این کلید باید همان کلید اصلی شما باشد
 const apiKey = "AIzaSyDmlac2OTGO1BDK08KVvLiDI5LeMcuWMDw";
@@ -13,12 +18,48 @@ async function testApiDirectly() {
   console.log("Starting direct API test with corrected payload structure...");
 
   try {
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      model: "text-embedding-004",
+      apiKey: apiKey,
+    });
+
     const response = await ai.models.embedContent({
       model: "text-embedding-004",
       contents: documents,
     });
 
-    const vectors = response.embeddings;
+    const vectors: any = response.embeddings;
+
+    const redisClient: any = createClient({
+      url: "redis://:ChRj72nuujSCW5z92XDVGitu@84.200.192.243:6379",
+    });
+
+    const vectorStore = new RedisVectorStore(embeddings, {
+      redisClient: redisClient,
+      indexName: "bluechart_blog_vectors",
+    });
+
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 50,
+    });
+
+    const rawDocs: Document[] = documents.map((post) => {
+
+
+      return new Document({
+        pageContent: post,
+        metadata: {
+          id: null,
+          title: post,
+          slug: post,
+        },
+      });
+    });
+
+    const chunkedDocuments: any[] = await splitter.splitDocuments(rawDocs);
+
+    await vectorStore.addVectors(vectors, chunkedDocuments);
 
     console.log("✅ API Call Successful. Vectors received: ", vectors);
   } catch (error) {
